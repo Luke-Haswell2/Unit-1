a7803a156cbebe4f7dedfa696566ef1a12239aea
Luke-Haswell2 <114390920+Luke-Haswell2@users.noreply.github.com>
2022-10-21 16:14:53 +0100

    menu script

---
+++ Assets/scripts/Menu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //put in bunker (push v for help)
+        //v = main menu
+        //c = controls
+        //b = back story
+    }
+}
