4d93cfef811adeabdf53ab71e081560907e4cc86
Luke-Haswell2 <114390920+Luke-Haswell2@users.noreply.github.com>
2022-10-31 14:45:06 +0000

    menu

--- Assets/scripts/Menu.cs
+++ Assets/scripts/Menu.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Menu : MonoBehaviour
 {
+    SpriteRenderer sr1;
+    SpriteRenderer sr2;
+    SpriteRenderer sr3;
+    public GameObject m1;
+    public GameObject m2;
+    public GameObject m3;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        sr1 = m1.GetComponent<SpriteRenderer>();
+        sr2 = m2.GetComponent<SpriteRenderer>();
+        sr3 = m3.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -17,5 +26,24 @@ public class Menu : MonoBehaviour
         //v = main menu
         //c = controls
         //b = back story
+        //x = start
+        if (Input.GetKeyDown("v"))
+        {
+            sr1.enabled = true;
+        }
+        if (Input.GetKeyDown("b"))
+        {
+            sr1.enabled = false;
+            sr2.enabled = true;
+        }
+        if (Input.GetKeyDown("c"))
+        {
+            sr2.enabled = false;
+            sr3.enabled = true;
+        }
+        if (Input.GetKeyDown("x"))
+        {
+            sr3.enabled = false;
+        }
     }
 }
