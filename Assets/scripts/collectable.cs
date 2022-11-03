77efabd54065c679cacafad6b060cb6717a97711
Luke-Haswell2 <114390920+Luke-Haswell2@users.noreply.github.com>
2022-11-03 11:49:27 +0000

    added objects

--- Assets/scripts/collectable.cs
+++ Assets/scripts/collectable.cs
@@ -2,17 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class collectible : MonoBehaviour
+public class Collectable : MonoBehaviour
 {
+    public GameObject end;
+    SpriteRenderer sr;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        sr = end.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         // create radio and make script delete player when they collect radio and spawn end of game text
+        
+
+    }
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+            sr.enabled = true;
+        }
     }
 }
